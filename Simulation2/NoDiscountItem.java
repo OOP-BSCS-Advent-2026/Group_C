58ec0dc71dbe91345549d60d6d6375879d1bed0f
Samie02-del <menhavictor@gmail.com>
2026-09-23 16:33:41 +0300

    Rename parameters in NoDiscountItem constructor
    Renamed the constructor parameters in NoDiscountItem from name and price to ItemName and ItemPrice to improve consistency and readability. Updated the super() call to use the new parameter names.

--- Simulation2/NoDiscountItem.java
+++ Simulation2/NoDiscountItem.java
@@ -1,6 +1,6 @@
 public class NoDiscountItem extends Item {
-    public NoDiscountItem(String name, double price) {
-        super(name, price);
+    public NoDiscountItem(String Itemname, double ItemPrice) {
+        super(Itemname, ItemPrice);
     }
 
     // No override needed - Item's calculateTotal() already does the right thing
